e97d75dca1c3734ac0dfb10fb2f536380ea64cd3
Naylanne Gomes <naylanne.lgc@gmail.com>
2025-11-30 12:58:43 -0300

    Update models.py

--- projeto4/projeto4/tarefa/models.py
+++ projeto4/projeto4/tarefa/models.py
@@ -2,11 +2,16 @@ from django.db import models
 from projeto.models import Projeto
 
 class Tarefa(models.Model):
+    PRIORIDADE_CHOICES = [
+        ("baixa", "Baixa"),
+        ("media", "Média"),
+        ("alta", "Alta"),
+    ]
     titulo = models.CharField(max_length=255)
     status = models.CharField(max_length=100)
-    prioridade = models.PositiveIntegerField()
+    prioridade = models.CharField(max_length=10, choices=PRIORIDADE_CHOICES)
     data_limite = models.DateTimeField()
     projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.titulo 
+        return self.titulo
